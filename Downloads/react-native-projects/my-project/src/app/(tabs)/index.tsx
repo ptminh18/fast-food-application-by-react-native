699e06487c90d1249bd83cc5b0860938d31a94df
first time with pycharm <mrblvck23@gmail.com>
2024-12-19 16:34:47 +0700

    added safe area view

--- Downloads/react-native-projects/my-project/src/app/(tabs)/index.tsx
+++ Downloads/react-native-projects/my-project/src/app/(tabs)/index.tsx
@@ -1,15 +1,17 @@
-import { View, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import products from '@assets/data/products';
 import ProductListItem from '@components/ProductListItems';
 
 export default function MenuScreen() {
   return (
+    <SafeAreaView>
       <FlatList data={products} 
       renderItem={({item}) => <ProductListItem product={item}/>}
       numColumns={2} 
-      contentContainerStyle={{ gap:10, paddingTop: 50}} 
+      contentContainerStyle={{ gap:10 }} 
       columnWrapperStyle={{ gap: 10, padding: 5 }}
       />
+    </SafeAreaView>
   );
 }
 
