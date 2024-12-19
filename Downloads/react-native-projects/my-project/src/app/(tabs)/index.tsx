707468bdb6b7707be575f2dd2eb714b9839ccea9
first time with pycharm <mrblvck23@gmail.com>
2024-12-19 13:06:49 +0700

    customing component

--- Downloads/react-native-projects/my-project/src/app/(tabs)/index.tsx
+++ Downloads/react-native-projects/my-project/src/app/(tabs)/index.tsx
@@ -1,53 +1,19 @@
-import { View, Image, Text, StyleSheet, Platform } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import products from '@/assets/data/products';
 import { HelloWave } from '@/src/components/HelloWave';
 import ParallaxScrollView from '@/src/components/ParallaxScrollView';
 import { ThemedText } from '@/src/components/ThemedText';
 import { ThemedView } from '@/src/components/ThemedView';
-import { Colors } from '@/src/constants/Colors';
+import ProductListItem from '@/src/components/ProductListItems';
 
-const product = products[0];
-
-export default function HomeScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image}} style={styles.image}/>
-
-
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[3]}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
 
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-  }, 
-  title: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    color: 'gray',
-    fontSize: 18,
-    fontWeight: 600,
-    marginVertical: 10,
-  },
-  price:{
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
 });
