9ebe0df9e1f9391c69141d733f9e227ec4ef742d
first time with pycharm <mrblvck23@gmail.com>
2024-12-19 16:07:45 +0700

    menu screen with flatlist

--- Downloads/react-native-projects/my-project/src/app/(tabs)/index.tsx
+++ Downloads/react-native-projects/my-project/src/app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import products from '@assets/data/products';
 import ProductListItem from '@components/ProductListItems';
 
 export default function MenuScreen() {
   return (
-    <View>
-      <ProductListItem product={products[3]}/>
-    </View>
+      <FlatList data={products} 
+      renderItem={({item}) => <ProductListItem product={item}/>}
+      numColumns={2} 
+      contentContainerStyle={{ gap:10, paddingTop: 50}} 
+      columnWrapperStyle={{ gap: 10, padding: 5 }}
+      />
   );
 }
 
--- Downloads/react-native-projects/my-project/src/components/ProductListItems.tsx
+++ Downloads/react-native-projects/my-project/src/components/ProductListItems.tsx
@@ -13,9 +13,10 @@ type ProductListItemsProps = {
 const ProductListItem = ({ product }: ProductListItemsProps) => {
     return (
       <View style={styles.container}>
-      <Image source={{ uri: product.image || defaultPizzaImage}} style={styles.image}/>
-  
-  
+      <Image source={{ uri: product.image || defaultPizzaImage}}
+       style={styles.image} 
+       resizeMode='contain'
+       />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
@@ -27,23 +28,25 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         padding: 10,
         borderRadius: 20,
+        flex: 1,
+        maxWidth: '50%',
     },
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-  }, 
-  title: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    color: 'gray',
-    fontSize: 18,
-    fontWeight: 600,
-    marginVertical: 10,
-  },
-  price:{
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-  },
+    image: {
+        width: '100%',
+        aspectRatio: 1,
+    }, 
+    title: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        color: 'gray',
+        fontSize: 18,
+        fontWeight: 600,
+        marginVertical: 10,
+    },
+    price:{
+        color: Colors.light.tint,
+        fontWeight: 'bold',
+    },
 })
 
   export default ProductListItem;
