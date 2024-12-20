741518ea0567087458b165fac39c97bbb1217057
first time with pycharm <mrblvck23@gmail.com>
2024-12-20 21:40:53 +0700

    shopping cart implementation and using modal

--- Downloads/react-native-projects/my-project/src/app/(tabs)/menu/_layout.tsx
+++ Downloads/react-native-projects/my-project/src/app/(tabs)/menu/_layout.tsx
@@ -1,9 +1,29 @@
-import { Stack } from 'expo-router';
+import { FontAwesome } from '@expo/vector-icons';
+import { Stack, Link } from 'expo-router';
+import { Pressable } from 'react-native';
+import Colors from '@/src/constants/Colors';
 
 export default function MenuStack() {
     return (
-    <Stack>
+    <Stack screenOptions={{
+        headerRight: () => (
+            <Link href="/cart" asChild>
+                <Pressable>
+                    {({ pressed }) => (
+                        <FontAwesome 
+                        name='shopping-cart' 
+                        size={25} 
+                        color={Colors.light.tint}
+                        style={{marginRight: 15, opacity: pressed ? 0.5 : 1}}
+                        />
+                    )}
+                </Pressable>
+            </Link>
+        )
+    
+    }}>
         <Stack.Screen name='index' options={{ title: 'Menu' }}/>
+
     </Stack>
     )
 }
